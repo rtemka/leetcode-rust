4caa50645539dddfd337d739d7bc1be04dcf259f
Artem Rybakov <rybakov333@gmail.com>
2023-11-14 15:50:20 +0300

    complete solution running-sum-of-1d-array

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,1 +1,2 @@
+pub mod running_sum_of_1d_array;
 pub mod two_sum_1;
---
+++ src/easy/running_sum_of_1d_array/mod.rs
@@ -0,0 +1,34 @@
+/// https://leetcode.com/problems/running-sum-of-1d-array
+struct Solution {}
+
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut result = Vec::with_capacity(nums.len());
+        let mut prev = 0;
+        for i in nums.into_iter() {
+            prev += i;
+            result.push(prev);
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use env_logger;
+    use log;
+
+    #[test]
+    fn running_sum() {
+        env_logger::init();
+        log::debug!("something to print in tests");
+
+        assert_eq!(vec![1, 3, 6], Solution::running_sum(vec![1, 2, 3]));
+
+        assert_eq!(
+            vec![3, 4, 6, 16, 17],
+            Solution::running_sum(vec![3, 1, 2, 10, 1])
+        );
+    }
+}
