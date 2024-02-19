c5be5dd6cd465a185957f9c8499ffa1f0e985204
rtemka <rybakov333@gmail.com>
2024-02-19 22:42:07 +0300

    added 383. Ransom note

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,4 +1,5 @@
 pub mod middle_of_linked_list;
+pub mod ransom_note;
 pub mod num_steps_to_reduce_num_to_zero;
 pub mod richest_customer_wealth;
 pub mod running_sum_of_1d_array;
---
+++ src/easy/ransom_note/mod.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+// https://leetcode.com/problems/ransom-note/
+impl Solution {
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        if magazine.len() < ransom_note.len() {
+            return false;
+        }
+        let mut m: HashMap<char, i32> = HashMap::new();
+        for char in magazine.chars() {
+            m.entry(char).and_modify(|v| *v += 1).or_insert(1);
+        }
+        for char in ransom_note.chars() {
+            match m.get_mut(&char) {
+                Some(count) if *count > 0 => *count -= 1,
+                _ => return false,
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ransom_note() {
+        assert_eq!(
+            true,
+            Solution::can_construct("aa".to_string(), "aab".to_string())
+        );
+        assert_eq!(
+            true,
+            Solution::can_construct("art".to_string(), "somethingaaaarrrrtttt".to_string())
+        );
+        assert_eq!(
+            false,
+            Solution::can_construct("xxxmmm".to_string(), "mmmassstt".to_string())
+        );
+    }
+}
