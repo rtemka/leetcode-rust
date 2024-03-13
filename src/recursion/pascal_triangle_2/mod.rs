739a736e6f75c6e29ef1f6697eb47d1f33b6bcb3
rtemka <rybakov333@gmail.com>
2024-03-13 22:36:50 +0300

    added pascal triangle II recursion variant

--- src/recursion/pascal_triangle_2/mod.rs
+++ src/recursion/pascal_triangle_2/mod.rs
@@ -1,8 +1,44 @@
 struct Solution {}
 
+// https://leetcode.com/problems/pascals-triangle-ii/description/
 impl Solution {
-    pub fn get_row(_row_index: i32) -> Vec<i32> {
-        Vec::new()
+    // recursion variant
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        if row_index == 0 {
+            return vec![1];
+        }
+        let prev = Self::get_row(row_index - 1);
+        let mut row = Vec::with_capacity(prev.len() + 1);
+        let mut j = 0;
+        while j <= prev.len() {
+            row.push(if j > 0 && j < prev.len() {
+                prev[j - 1] + prev[j]
+            } else {
+                1
+            });
+            j += 1;
+        }
+        row
+    }
+
+    pub fn get_row2(row_index: i32) -> Vec<i32> {
+        if row_index == 0 {
+            return vec![1];
+        }
+        let mut row = Vec::with_capacity(row_index as usize + 1);
+        for _ in 0..row_index + 1 {
+            // println!("{:?}", row);
+            let mut prev = 0;
+            let mut j = 0;
+            let len = row.len();
+            while j <= len {
+                println!("{:?},{}", row, len);
+                row.push(if j > 0 && j < len { prev + row[j] } else { 1 });
+                prev = row[j];
+                j += 1;
+            }
+        }
+        row
     }
 }
 
@@ -12,8 +48,11 @@ mod tests {
 
     #[test]
     fn pascal_triangle2() {
+        assert_eq!(vec![1], Solution::get_row2(0));
+        assert_eq!(vec![1, 1], Solution::get_row2(1));
+        assert_eq!(vec![1, 2, 1], Solution::get_row(2));
         assert_eq!(vec![1, 3, 3, 1], Solution::get_row(3));
-        assert_eq!(vec![1], Solution::get_row(0));
-        assert_eq!(vec![1, 1], Solution::get_row(1));
+        assert_eq!(vec![1, 4, 6, 4, 1], Solution::get_row(4));
+        assert_eq!(vec![1, 5, 10, 10, 5, 1], Solution::get_row(5));
     }
 }
