04484c30f8cb608f318b8a7bbd273c829a318fb4
rtemka <rybakov333@gmail.com>
2024-03-20 14:07:39 +0300

    started max depth of binary tree

---
+++ src/recursion/max_depth_of_binary_tree/mod.rs
@@ -0,0 +1,61 @@
+struct Solution {}
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(_n) => 1,
+            None => 0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn max_depth_of_bst() {
+        let bst = Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 7,
+                left: None,
+                right: None,
+            }))),
+        }));
+        assert_eq!(2, Solution::max_depth(Some(bst)));
+    }
+}
--- src/recursion/mod.rs
+++ src/recursion/mod.rs
@@ -1,5 +1,6 @@
 pub mod climbing_stairs;
 pub mod fibonacci_number;
+pub mod max_depth_of_binary_tree;
 pub mod pascal_triangle_2;
 pub mod reverse_linked_list;
 pub mod reverse_string;
