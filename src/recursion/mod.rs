1dab4f01cd448081a74a9f77b85c0c78ef4718d1
rtemka <rybakov333@gmail.com>
2024-03-21 22:00:39 +0300

    added 50. pow(x,n) - not all corner cases polished in my version of pow

--- src/recursion/mod.rs
+++ src/recursion/mod.rs
@@ -2,6 +2,7 @@ pub mod climbing_stairs;
 pub mod fibonacci_number;
 pub mod max_depth_of_binary_tree;
 pub mod pascal_triangle_2;
+pub mod pow_x_n;
 pub mod reverse_linked_list;
 pub mod reverse_string;
 pub mod search_in_bst;
---
+++ src/recursion/pow_x_n/mod.rs
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/powx-n/description/
+struct Solution {}
+
+impl Solution {
+    pub fn my_pow_std(x: f64, n: i32) -> f64 {
+        x.powi(n)
+    }
+    pub fn my_pow_rec(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            return 1.0;
+        }
+        if x == 1.0 {
+            return x;
+        }
+
+        if n < 0 {
+            1.0 / Self::my_pow_rec_helper(x, n * -1)
+        } else {
+            Self::my_pow_rec_helper(x, n)
+        }
+    }
+
+    pub fn my_pow_rec_helper(x: f64, n: i32) -> f64 {
+        if n == 1 {
+            x
+        } else {
+            x * Self::my_pow_rec(x, n - 1) // tail call?
+        }
+    }
+
+    pub fn my_pow_loop(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            return 1.0;
+        }
+        if x == 1.0 {
+            return x;
+        }
+        let (n, negative) = if n < 0 { (n * -1, true) } else { (n, false) };
+        let mut res = x;
+        println!("{},{}", n, negative);
+        for _ in 1..n {
+            println!("{}", res);
+            res *= x;
+        }
+        if negative {
+            1.0 / res
+        } else {
+            res
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn my_pow() {
+        assert_eq!(1024.0000, Solution::my_pow_std(2.0000, 10));
+        assert_eq!(1024.0000, Solution::my_pow_rec(2.0000, 10));
+        // assert_eq!(9.2610, Solution::my_pow_rec(2.1000, 3));
+        assert_eq!(0.2500, Solution::my_pow_rec(2.0000, -2));
+        assert_eq!(1024.0000, Solution::my_pow_loop(2.0000, 10));
+        assert_eq!(1.0000, Solution::my_pow_loop(1.0000, 2147483647));
+    }
+}
