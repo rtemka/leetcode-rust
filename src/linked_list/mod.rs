7c4c1804c67f663de07bb11cd7647d102a77a481
rtemka <rybakov333@gmail.com>
2024-05-11 22:44:50 +0300

    added 234. palindrome linked list

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,4 +1,5 @@
 pub mod design_linked_list;
 pub mod odd_even_linked_list;
+pub mod palindrome_linked_list;
 pub mod remove_linked_list_elements;
 pub mod remove_nth_node_from_end_of_list;
---
+++ src/linked_list/palindrome_linked_list/mod.rs
@@ -0,0 +1,80 @@
+// https://leetcode.com/problems/palindrome-linked-list/description/
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+// https://leetcode.com/problems/palindrome-linked-list/description/
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+        Self::is_palindrome_helper(&head, &head).is_ok()
+    }
+
+    fn is_palindrome_helper<'a>(
+        tail: &'a Option<Box<ListNode>>,
+        head: &'a Option<Box<ListNode>>,
+    ) -> Result<&'a Option<Box<ListNode>>, ()> {
+        match tail {
+            Some(n) => match Self::is_palindrome_helper(&n.next, head)? {
+                Some(hn) => {
+                    if n.val == hn.val {
+                        Ok(&hn.next)
+                    } else {
+                        Err(())
+                    }
+                }
+                _ => Err(()),
+            },
+            None => Ok(head),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn linked_list_is_palindrome() {
+        let head = ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 3,
+                        next: Some(Box::new(ListNode {
+                            val: 2,
+                            next: Some(Box::new(ListNode { val: 1, next: None })),
+                        })),
+                    })),
+                })),
+            })),
+        };
+
+        assert!(Solution::is_palindrome(Some(Box::new(head))));
+
+        assert!(!Solution::is_palindrome(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode { val: 2, next: None }))
+        }))));
+        // assert!(!Solution::is_palindrome(Some(Box::new(head))));
+
+        assert!(Solution::is_palindrome(Some(Box::new(ListNode {
+            val: 1,
+            next: None
+        }))));
+    }
+}
