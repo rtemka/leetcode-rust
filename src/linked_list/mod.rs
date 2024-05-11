b055729c61cec7cb0f12ac829ce2531250746346
rtemka <rybakov333@gmail.com>
2024-05-11 16:21:27 +0300

    added 328. odd even linked list

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,3 +1,4 @@
 pub mod design_linked_list;
+pub mod odd_even_linked_list;
 pub mod remove_linked_list_elements;
 pub mod remove_nth_node_from_end_of_list;
---
+++ src/linked_list/odd_even_linked_list/mod.rs
@@ -0,0 +1,76 @@
+// https://leetcode.com/problems/odd-even-linked-list/description/
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn odd_even_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut odd, mut even) = (head, Box::new(ListNode::new(-1)));
+        let (mut e, mut o) = (&mut even, &mut odd);
+
+        while let Some(n) = o {
+            e.next = n.next.take();
+            // println!("{n:?}\n{:?}", e);
+            if let Some(ref mut nn) = e.next {
+                n.next = nn.next.take();
+                e = nn;
+            }
+            if n.next.is_some() {
+                o = &mut n.next;
+            } else {
+                n.next = even.next;
+                break;
+            }
+        }
+        odd
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn odd_even_list() {
+        let head = ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode {
+                            val: 5,
+                            next: Some(Box::new(ListNode {
+                                val: 6,
+                                next: Some(Box::new(ListNode { val: 7, next: None })),
+                            })),
+                        })),
+                    })),
+                })),
+            })),
+        };
+
+        let mut answer = Solution::odd_even_list(Some(Box::new(head)));
+        println!("{answer:?}");
+        for n in vec![1, 3, 5, 7, 2, 4, 6] {
+            println!("{n}");
+            let node = answer.unwrap();
+            assert_eq!(n, node.val);
+            answer = node.next;
+        }
+    }
+}
