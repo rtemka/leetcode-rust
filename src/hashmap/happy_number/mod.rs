8b0d8e5976825f316246ff3e38439bd499b83eba
rtemka <rybakov333@gmail.com>
2024-06-01 21:59:02 +0300

    added 202. happy number

---
+++ src/hashmap/happy_number/mod.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+
+// https://leetcode.com/problems/happy-number/description/
+struct Solution;
+
+impl Solution {
+    pub fn is_happy(mut n: i32) -> bool {
+        let mut set = HashSet::new();
+        let mut rem;
+        let mut square_sum;
+        loop {
+            square_sum = 0;
+            while n > 0 {
+                rem = n % 10;
+                square_sum += rem * rem;
+                n /= 10;
+            }
+            if square_sum == 1 {
+                return true;
+            }
+            if !set.insert(square_sum) {
+                break;
+            }
+            n = square_sum;
+        }
+        false
+    }
+
+    // solution with cycle detection
+    // where fast and slow pointers is used.
+    pub fn is_happy_fast_slow_pointers(n: i32) -> bool {
+        let mut slow = n;
+        let mut fast = Solution::nxt(n);
+        while fast != 1 && slow != fast {
+            slow = Solution::nxt(slow);
+            fast = Solution::nxt(Solution::nxt(fast));
+        }
+        return fast == 1;
+    }
+
+    fn nxt(mut n: i32) -> i32 {
+        let mut res = 0;
+        while n > 0 {
+            let d = n % 10;
+            res += d * d;
+            n /= 10;
+        }
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_happy() {
+        assert!(Solution::is_happy(19));
+        assert!(!Solution::is_happy(3));
+        assert!(!Solution::is_happy(2));
+        assert!(!Solution::is_happy(46));
+    }
+}
--- src/hashmap/mod.rs
+++ src/hashmap/mod.rs
@@ -1,3 +1,4 @@
 pub mod contains_duplicates;
 pub mod design_hash_set;
+pub mod happy_number;
 pub mod intersection_of_2_arrays;
