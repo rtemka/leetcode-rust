92309449357cf17bcfb90fb0b6ea665ea68f8519
rtemka <rybakov333@gmail.com>
2024-08-19 22:18:14 +0300

    added 264. ugly number 2

--- src/dp/ugly_number_2/mod.rs
+++ src/dp/ugly_number_2/mod.rs
@@ -3,14 +3,28 @@ struct Solution;
 
 impl Solution {
     pub fn nth_ugly_number(n: i32) -> i32 {
-        if n == 1 {
-            return 1;
+        let n = n as usize;
+        let mut v = Vec::with_capacity(n);
+        v.push(1);
+        let mut i = 1;
+        let (mut m2, mut m3, mut m5) = (0, 0, 0);
+        while i < n {
+            let two_multiple = v[m2] * 2;
+            let three_multiple = v[m3] * 3;
+            let five_multiple = v[m5] * 5;
+
+            v.push(i32::min(
+                two_multiple,
+                i32::min(three_multiple, five_multiple),
+            ));
+
+            m2 = if v[i] == two_multiple { m2 + 1 } else { m2 };
+            m3 = if v[i] == three_multiple { m3 + 1 } else { m3 };
+            m5 = if v[i] == five_multiple { m5 + 1 } else { m5 };
+
+            i += 1;
         }
-        let mut two = vec![2];
-        let mut three = vec![3];
-        let mut five = vec![5];
-        for _ in 0..n {}
-        -1
+        v[n - 1]
     }
 }
 
