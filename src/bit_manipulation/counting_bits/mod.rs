137abd191f936f614f0c54384da67077d411fe34
rtemka <rybakov333@gmail.com>
2024-08-23 21:45:54 +0300

    added 338. counting bits

---
+++ src/bit_manipulation/counting_bits/mod.rs
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/counting-bits/description
+struct Solution;
+
+// Brian Kernigan apporach (O(logN)): https://stackoverflow.com/a/12381102
+
+impl Solution {
+    pub fn count_bits(n: i32) -> Vec<i32> {
+        let mut ans = Vec::with_capacity(n as usize + 1);
+        for i in 0..n {
+            ans.push(Self::count_set_bits(i));
+        }
+        ans
+    }
+
+    #[inline]
+    const fn count_set_bits(mut n: i32) -> i32 {
+        let mut count = 0;
+        while n > 0 {
+            n &= n - 1;
+            count += 1;
+        }
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count_bits() {
+        assert_eq!(vec![0, 1, 1], Solution::count_bits(2));
+    }
+}
--- src/bit_manipulation/mod.rs
+++ src/bit_manipulation/mod.rs
@@ -1,2 +1,3 @@
+pub mod counting_bits;
 pub mod power_of_two;
 pub mod single_number;
