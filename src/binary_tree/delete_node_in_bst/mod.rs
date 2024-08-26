fc05e5de118f9fe3d0e4196b6aece759fa9fbdba
rtemka <rybakov333@gmail.com>
2024-08-26 22:23:32 +0300

    added 405. delete node in bst

--- src/binary_tree/delete_node_in_bst/mod.rs
+++ src/binary_tree/delete_node_in_bst/mod.rs
@@ -1,16 +1,107 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/description
 struct Solution;
 
 use crate::binary_tree::TreeNode;
 use std::cell::RefCell;
+use std::ops::Deref;
 use std::rc::Rc;
 impl Solution {
     pub fn delete_node(
-        root: Option<Rc<RefCell<TreeNode>>>,
+        mut root: Option<Rc<RefCell<TreeNode>>>,
         key: i32,
     ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(_) = Self::delete_node_helper(&mut root, key) {
+            None
+        } else {
+            root
+        }
+    }
+
+    pub fn delete_node_helper(root: &mut Option<Rc<RefCell<TreeNode>>>, key: i32) -> Option<()> {
         match root {
-            Some(_) => root,
+            Some(node) if node.borrow_mut().val != key => {
+                let mut node = node.borrow_mut();
+                if node.val > key {
+                    if let Some(_) = Self::delete_node_helper(&mut node.left, key) {
+                        node.left = None;
+                        None
+                    } else {
+                        None
+                    }
+                } else {
+                    if let Some(_) = Self::delete_node_helper(&mut node.right, key) {
+                        node.right = None;
+                        None
+                    } else {
+                        None
+                    }
+                }
+            }
+            Some(node) => {
+                let (n, swap) = {
+                    let mut n = node.borrow_mut();
+                    if n.left.is_some() && n.right.is_none() {
+                        (n.left.take(), true)
+                    } else if n.right.is_some() && n.left.is_none() {
+                        (n.right.take(), true)
+                    } else {
+                        (Self::take_successor(&mut n.right), false)
+                    }
+                };
+                if let Some(n) = n {
+                    if swap {
+                        node.swap(n.deref());
+                    } else {
+                        let mut node = node.borrow_mut();
+                        let mut n = n.borrow_mut();
+                        node.val = n.val;
+                        if n.right.is_some() {
+                            Self::attach_to_right(&mut n.right, node.right.take());
+                            node.right = n.right.take();
+                        }
+                    }
+                    None
+                } else {
+                    Some(())
+                }
+            }
             None => None,
+        }
+    }
+
+    fn take_successor(root: &mut Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let node = match root {
+            Some(node) => {
+                let mut node = node.borrow_mut();
+                if node.left.is_some() {
+                    Self::take_successor(&mut node.left)
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+        if node.is_some() {
+            node
+        } else {
+            root.take()
+        }
+    }
+
+    fn attach_to_right(
+        root: &mut Option<Rc<RefCell<TreeNode>>>,
+        node_to_attach: Option<Rc<RefCell<TreeNode>>>,
+    ) {
+        match root {
+            Some(node) => {
+                let mut node = node.borrow_mut();
+                if node.right.is_none() {
+                    node.right = node_to_attach;
+                } else {
+                    Self::attach_to_right(&mut node.right, node_to_attach);
+                }
+            }
+            None => (),
         }
     }
 }
@@ -46,7 +137,8 @@ mod tests {
                 }))),
             }))),
         })));
-        let _ans = Solution::delete_node(root, 3);
+        let ans = Solution::delete_node(root, 3);
+        assert!(ans.is_some());
         let _expected = vec![5, 4, 6, 2, 7];
     }
 }
