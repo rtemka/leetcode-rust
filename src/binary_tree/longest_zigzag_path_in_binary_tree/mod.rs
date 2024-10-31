5d71571b11a7f16484b86d6225f9597046ac49e3
rtemka <rybakov333@gmail.com>
2024-10-31 22:14:10 +0300

    added 1372. longest zigzag path in binary tree

--- src/binary_tree/longest_zigzag_path_in_binary_tree/mod.rs
+++ src/binary_tree/longest_zigzag_path_in_binary_tree/mod.rs
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description
 struct Solution;
 
 use crate::binary_tree::TreeNode;
@@ -16,16 +17,16 @@ impl Solution {
         cur: i32,
         max: &mut i32,
     ) {
-        *max = i32::max(*max, cur);
         match root {
             Some(node) => {
+                *max = i32::max(*max, cur);
                 let node = node.borrow();
                 if dir_left {
                     Self::longest_zig_zag_rec(&node.right, false, cur + 1, max);
-                    Self::longest_zig_zag_rec(&node.left, true, 0, max);
+                    Self::longest_zig_zag_rec(&node.left, true, 1, max);
                 } else {
                     Self::longest_zig_zag_rec(&node.left, true, cur + 1, max);
-                    Self::longest_zig_zag_rec(&node.right, false, 0, max);
+                    Self::longest_zig_zag_rec(&node.right, false, 1, max);
                 }
             }
             None => (),
